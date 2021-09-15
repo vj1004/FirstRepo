2f4b688d4cfd6330b9cff057942f69e7c47675ef
StealtherThreat <princetushargoyal@gmail.com>
2021-09-15 14:39:53 +0530

    Added get-set

--- LtiAirlines/src/main/java/com/lti/model/Passenger.java
+++ LtiAirlines/src/main/java/com/lti/model/Passenger.java
@@ -2,6 +2,35 @@ package com.lti.model;
 
 public class Passenger {
 	
-	Flight f = new Flight(1002, "Delhi", "Mumbai");
+	int passengerId;
+	String passengerName;
+	String passengerAddresss;
+	int age;
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public String getPassengerName() {
+		return passengerName;
+	}
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	public String getPassengerAddresss() {
+		return passengerAddresss;
+	}
+	public void setPassengerAddresss(String passengerAddresss) {
+		this.passengerAddresss = passengerAddresss;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 
 }
